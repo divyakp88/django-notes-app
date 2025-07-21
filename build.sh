2084aa98604cc228fcecab1c586da0c37bf4b1c8
divyakp88 <kpdivya08@gmail.com>
2025-07-21 20:40:39 +0530

    super user added

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
 pip install -r requirements.txt
 python manage.py migrate
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='divya').exists() or User.objects.create_superuser('divya', 'kpdivya08@gmail.com', '123456')" | python manage.py shell
+python manage.py collectstatic --noinput
